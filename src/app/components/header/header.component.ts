e6d734d623b021dd70fcd22cdb26b6d559a3035d
danielnoo <dannynoo@gmail.com>
2021-11-28 20:39:38 -0500

    working on form component for login

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -6,10 +6,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
-
+  showLogin: boolean = false;
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onClick () {
+    this.showLogin = !this.showLogin
   }
 
 }
---
+++ src/app/components/login-form/login-form.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-login-form',
+  templateUrl: './login-form.component.html',
+  styleUrls: ['./login-form.component.scss']
+})
+export class LoginFormComponent implements OnInit {
+  showSignIn: boolean = true;
+  signInName: string;
+  signInPassword: string;
+  
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  onSignIn () {
+    console.log('sign in attempt');
+    
+  }
+}
